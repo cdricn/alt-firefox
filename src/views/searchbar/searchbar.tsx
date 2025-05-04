b0a454a2d91606ec6fd41e7ea0dfda6303b0a213
cdricn <cdricncdricn@gmail.com>
2025-05-04 22:16:28 +0800

    Drag test

--- src/views/searchbar/searchbar.tsx
+++ src/views/searchbar/searchbar.tsx
@@ -1,16 +1,43 @@
-import './searchbar.scss'
+import './searchbar.scss';
+import { SlCursorMove, SlSocialGoogle } from "react-icons/sl";
 
 export default function SearchBar() {
   
   const styles = {
     width: "400px",
-    height: "600px",
-    border: "2px solid white"
+    minWidth: "fit-content",
+    height: "fit-content",
+    border: "2px solid white",
+    overflow: "scroll"
   }
+
+  let cursorPosX = 0;
+  let cursorPosY = 0;
+  let newCursorPosX = 0;
+  let newCursorPosY = 0;
+
+  const dragElement = (e:any) => {
+    if (true) {
+      document.getElementById(e)!.onmousedown = dragMouseDown;
+    } else {
+      
+    }
+  }
+
+  const dragMouseDown = (e:any) => {
+    cursorPosX = e.clientX;
+    cursorPosY = e.clientY;
+  }
+
 
   return (
     <div className='window-container' style={styles}>
-      
+      <SlCursorMove id='btn-drag' />
+      <button className='search-bar'>
+        <SlSocialGoogle className='search-logo'/>
+        <input className='textbox'>
+        </input>
+      </button>
     </div>
   )
 }
